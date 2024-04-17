a2d964bfea05c2a59c74f4ee8e9043a5f71d7f5a
T189216 <tus0926@naver.com>
2024-04-17 17:09:26 +0900

    chore: 도커 이미지 생성 자동화

--- src/main/java/com/ll/tagging/domain/controller/HomeController.java
+++ src/main/java/com/ll/tagging/domain/controller/HomeController.java
@@ -1,15 +1,24 @@
 package com.ll.tagging.domain.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HomeController {
+    @Value("${custom.jwt.secretKey}")
+    private String jwtSecretKey;
 
     @GetMapping("/")
     @ResponseBody
     public String home() {
-        return "홈";
+        return "홈2";
+    }
+
+    @GetMapping("/jwtSecretKey")
+    @ResponseBody
+    public String showJwtSecretKey() {
+        return jwtSecretKey;
     }
 }
