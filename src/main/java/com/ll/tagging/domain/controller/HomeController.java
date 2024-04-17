d139d3267eae7e3e5680008d91b7efa4b912906a
T189216 <tus0926@naver.com>
2024-04-17 15:57:53 +0900

    중단 배포용 테스트 변경

--- src/main/java/com/ll/tagging/domain/controller/HomeController.java
+++ src/main/java/com/ll/tagging/domain/controller/HomeController.java
@@ -2,12 +2,14 @@ package com.ll.tagging.domain.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HomeController {
 
     @GetMapping("/")
+    @ResponseBody
     public String home() {
-        return "domain/home";
+        return "홈";
     }
 }
